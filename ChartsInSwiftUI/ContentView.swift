e80bc2e44e6459c03d62a8810255b85934cf45cb
Shireesh Marla <shireesh.marla@swipiicard.com>
2021-11-29 13:14:19 +0000

    Content view changes

--- ChartsInSwiftUI/ContentView.swift
+++ ChartsInSwiftUI/ContentView.swift
@@ -6,11 +6,34 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+//            Spacer()
+//            LineView(data: [10,12,5,3,2,1,20,25], title: "Line Chart")
+            Spacer()
+            //Line chart
+            LineChartView(data: [10,12,5,3,2,1,20,25], title: "Line Chart")
+            Spacer()
+            //Bar Chart
+            BarChartView(data: ChartData(values: [
+                ("Jan", 12),
+                ("Feb", 6),
+                ("Mar", 18),
+                ("Apr", 11),
+                ("May", 6),
+                ("Jun", 9),
+                ("Jul", 20),
+                ("Aug", 13)
+            ]), title: "Bar Chart")
+            Spacer()
+            //Pie Chart
+            PieChartView(data: [2,2,4,6,12,25,15], title: "Pie Chart")
+            Spacer()
+            
+        }
     }
 }
 
