a17dc50da4518e829b23db5a27dce0b5eebff328
Shireesh Marla <shireesh.marla@swipiicard.com>
2021-11-29 11:57:23 +0000

    Initial Commit

---
+++ ChartsInSwiftUI/ChartsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsInSwiftUIApp.swift
+//  ChartsInSwiftUI
+//
+//  Created by Shireesh Marla on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChartsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartsInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChartsInSwiftUI
+//
+//  Created by Shireesh Marla on 29/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
